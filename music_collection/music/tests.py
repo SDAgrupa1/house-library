a0e718d9f90e469032640000fdd87514babb65fa
Natalia Czapska <czapskanatalia@gmail.com>
2021-08-25 23:25:31 +0200

    Add str representation for name_cd

--- music_collection/music/tests.py
+++ music_collection/music/tests.py
@@ -10,3 +10,7 @@ class MusicModelTest(TestCase):
     def test_string_representation_for_performer(self):
         music = Music(performer="Madonna")
         self.assertEqual(str(music), music.performer)
+
+    def test_string_representation_for_name_cd(self):
+        music = Music(name_cd="Like a Prayer")
+        self.assertEqual(str(music), music.name_cd)
