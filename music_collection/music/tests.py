ce0a9ba8ade7ed3b2ab33fd6e2f35c57e49fa598
Natalia Czapska <czapskanatalia@gmail.com>
2021-08-25 23:24:06 +0200

    Added str representation for performer

--- music_collection/music/tests.py
+++ music_collection/music/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Music
+
+"""To run all tests type python manage.py test music"""
+
+
+class MusicModelTest(TestCase):
+
+    def test_string_representation_for_performer(self):
+        music = Music(performer="Madonna")
+        self.assertEqual(str(music), music.performer)
