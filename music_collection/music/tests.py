2bba723f54d95ba06ec592d1741a4fab25562b7a
Natalia Czapska <czapskanatalia@gmail.com>
2021-08-25 23:26:20 +0200

    Add str representation for publisher

--- music_collection/music/tests.py
+++ music_collection/music/tests.py
@@ -14,3 +14,7 @@ class MusicModelTest(TestCase):
     def test_string_representation_for_name_cd(self):
         music = Music(name_cd="Like a Prayer")
         self.assertEqual(str(music), music.name_cd)
+
+    def test_string_representation_for_publisher(self):
+        music = Music(publisher="Sire Records")
+        self.assertEqual(str(music), music.publisher)
