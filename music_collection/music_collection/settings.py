1d422ee5f84420b56eba4c8be00e886b698bc78e
Bartosz <bartosz.kabala95@gmail.com>
2021-08-26 17:35:25 +0200

    Create user and superuser

--- music_collection/music_collection/settings.py
+++ music_collection/music_collection/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+
 import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "music",
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
