eda907b429c6d9712e2e4a0332d3f0846663a8ff
Bartosz <bartosz.kabala95@gmail.com>
2021-08-29 19:33:51 +0200

    BK-40-Edit-Profile, Add profile view, user can change password, user can set user's details

--- music_collection/accounts/forms.py
+++ music_collection/accounts/forms.py
@@ -1,63 +1,70 @@
-from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
 
 User = get_user_model()
 
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}), max_length=255)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),max_length=50)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),max_length=50)
 
     class Meta:
         model = User
-        fields = ['email']
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        qs = User.objects.filter(email=email)
-        if qs.exists():
-            raise forms.ValidationError("email is taken")
-        return email
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password_2 = cleaned_data.get("password_2")
-        if password is not None and password != password_2:
-            self.add_error("password_2", "Your passwords must match")
-        return cleaned_data
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'password2']
 
 
-class UserAdminCreationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}), max_length=255)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),max_length=50)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),max_length=50)
 
     class Meta:
         model = User
-        fields = ['email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'password2']
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password_2 = cleaned_data.get("password_2")
-        if password is not None and password != password_2:
-            self.add_error("password_2", "Your passwords must match")
-        return cleaned_data
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
 
 
-class UserAdminChangeForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField()
+class EditProfileView(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}), max_length=255)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), max_length=50)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), max_length=50)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), max_length=50)
+    last_login = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), max_length=50)
+    is_superuser = forms.CharField(widget=forms.CheckboxInput(attrs={'class': 'form-check'}), max_length=50)
+    is_staff = forms.CharField(widget=forms.CheckboxInput(attrs={'class': 'form-check'}), max_length=50)
+    is_active = forms.CharField(widget=forms.CheckboxInput(attrs={'class': 'form-check'}), max_length=50)
+    date_joined = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), max_length=50)
+
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'is_active', 'admin']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'last_login', 'is_superuser', 'is_staff',
+                  'is_active', 'date_joined']
 
-    def clean_password(self):
-        return self.initial["password"]
+class EditForm():
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}), max_length=255)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),max_length=50)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),max_length=50)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'password2']
+
+
+class PasswordChangingForm(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
+
+    class Meta:
+        model = User
+        fields = ['old_password', 'new_password1', 'new_password2']
--- music_collection/accounts/views.py
+++ music_collection/accounts/views.py
@@ -1,10 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.views import generic
+from .forms import SignUpForm, RegisterForm, EditProfileView, PasswordChangingForm
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    success_url = reverse_lazy('password_success')
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileView
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
