a2a4adc8247e6a3d7c2ccbdd099724f9490f9951
blazej <blazej@flash-group.pl>
2021-08-30 13:26:14 +0200

    refactor class Music to MusicAlbum

--- music_collection/music/admin.py
+++ music_collection/music/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Music, Category, Availability, Rating
+from .models import MusicAlbum, Category, Availability, Rating
 
 
-admin.site.register(Music)
+admin.site.register(MusicAlbum)
 admin.site.register(Category)
 admin.site.register(Availability)
 admin.site.register(Rating)
--- music_collection/music/tests.py
+++ music_collection/music/tests.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from .models import Music
+from .models import MusicAlbum
 
 """To run all tests type python manage.py test music"""
 
@@ -10,55 +10,55 @@ class MusicModelTest(TestCase):
 
     def setUp(self):
         # Set up non-modified object used by test methods
-        Music.objects.create(performer="Madonna",
-                             name_cd="Like a Prayer",
-                             publisher="Sire Records",
-                             year="1989-01-01",
-                             info="This is some example track list",
-                             category_models=None
-                             )
+        MusicAlbum.objects.create(performer="Madonna",
+                                  name_cd="Like a Prayer",
+                                  publisher="Sire Records",
+                                  year="1989-01-01",
+                                  info="This is some example track list",
+                                  category_models=None
+                                  )
 
     # test for labels fields
     def test_performer_label(self):
-        music = Music.objects.get(id=1)
+        music = MusicAlbum.objects.get(id=1)
         field_label = music._meta.get_field("performer").verbose_name
         self.assertEqual(field_label, "performer")
 
     def test_name_cd_label(self):
-        music = Music.objects.get(id=1)
+        music = MusicAlbum.objects.get(id=1)
         field_label = music._meta.get_field("name_cd").verbose_name
         self.assertEqual(field_label, "name cd")
 
     def test_publisher_label(self):
-        music = Music.objects.get(id=1)
+        music = MusicAlbum.objects.get(id=1)
         field_label = music._meta.get_field("publisher").verbose_name
         self.assertEqual(field_label, "publisher")
 
     def test_year_label(self):
-        music = Music.objects.get(id=1)
+        music = MusicAlbum.objects.get(id=1)
         field_label = music._meta.get_field("year").verbose_name
         self.assertEqual(field_label, "year")
 
     def test_info_label(self):
-        music = Music.objects.get(id=1)
+        music = MusicAlbum.objects.get(id=1)
         field_label = music._meta.get_field("info").verbose_name
         self.assertEqual(field_label, "info")
 
     # test str representation
     def test_string_representation(self):
-        music = Music.objects.get(id=1)
+        music = MusicAlbum.objects.get(id=1)
         performer_field = music.performer
         name_cd_field = music.name_cd
         self.assertEqual(str(music), f"{performer_field} {name_cd_field}")
 
     # test str in date field
     def test_str_in_year_should_fail(self):
-        music = Music(year="Two thousand ten")
+        music = MusicAlbum(year="Two thousand ten")
         with self.assertRaises(ValidationError):
             music.save()
 
     # test max len for performer field
     def test_performer_name_max_length(self):
-        music = Music.objects.get(id=1)
+        music = MusicAlbum.objects.get(id=1)
         max_length = music._meta.get_field('performer').max_length
         self.assertEqual(max_length, 64)
