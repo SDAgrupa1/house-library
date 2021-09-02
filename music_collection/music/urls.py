512a933f7e6c398d7e717cb0123326a70c79935f
Natalia Czapska <czapskanatalia@gmail.com>
2021-09-02 19:53:29 +0200

    NC-front: Cosmetic updates.

--- music_collection/music/urls.py
+++ music_collection/music/urls.py
@@ -7,9 +7,9 @@ app_name = 'music-urls'
 urlpatterns = [
     path('cds/', views.CdTemplateView.as_view(), name='list-of-cds'),
     path('cds/<id>/', views.cds_dynamic_lookup_view, name='cd-bio'),
-    path('rate/',views.rate_music, name='rate'),
+    path('rate/', views.rate_music, name='rate'),
     path('all_music_albums/', all_music_albums, name='all_music_albums'),
     path('new/', views.new_music_album, name='new'),
-    path('edit/<int:id>', views.edit_music_album, name='edit'),
-    path('delete/<int:id>', views.delete_music_album, name='delete')
+    path('edit/<int:id>/', views.edit_music_album, name='edit'),
+    path('delete/<int:id>/', views.delete_music_album, name='delete'),
 ]
